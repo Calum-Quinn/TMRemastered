2ad17c8d37b21b3590b730dfae83abdadfffc8a6
calum <calum@quinns.ch>
2024-03-07 09:37:52 +0100

    Set up project

---
+++ Code/Encryption.java
@@ -0,0 +1,12 @@
+package MonCode.Code;
+
+import java.util.*;
+
+public class Encryption {
+
+    private static Scanner sc;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
